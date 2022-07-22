63b594d352505f2bb3a9f9eee72b5f46eddfb294
Sec Ghost <sec666host@gmail.com>
2022-07-22 13:44:47 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,484 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3939850173581632" crossorigin="anonymous"></script>
+<meta name="google-site-verification" content="8XaLaVojmEDyNS22m_6sUBOKXsbmnxlYl_onXs_pA_M" />
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Property in Lucknow, 2, 3, 4 BHK Flats for Sale, Apartment in Lucknow – Shalimar Corp Limited </title>
+
+<meta name="description" content="Shalimar Corp Limited is a renowned real estate development company in Lucknow that offers luxury 2, 3 & 4 BHK flats, luxury apartments, villas, plots, row houses in Lucknow. For Booking Contact us" />
+
+<meta name="keywords" content="Property in Lucknow, 2 BHk Flats in Lucknow, 3 BHk Flats in Lucknow, 4 BHK Flats in Lucknow, Apartments in Lucknow, Villas in Lucknow, Plots in Lucknow, Row Houses in Lucknow" />
+
+<link rel="canonical" href="https://www.shalimarcorp.com"/>
+
+<link rel="alternate" hreflang="en-us"/>
+
+<meta name="Owner" content="shalimarcorp.com"/>
+
+<meta name="language" content="EN" />
+
+<meta name="robots" content="All" />
+
+<meta name="Copyright" content="© ShalimarCorp"/>
+
+<meta name="classification" content="Real Estate Property in Lucknow"/>
+
+<meta name="subject" content="upcoming residential projects in lucknow, Search New Projects, Builders And Property in Lucknow"/>
+
+<meta name="author" content="http://www.shalimarcorp.com" />
+
+<meta name="page-topic" content="Top Real Estate Companies in Lucknow" />
+
+<meta name="page-type" content="Information">
+
+<meta name="robots" content="index, follow" />
+
+<meta name="googlebot" content=" index, follow " />
+
+<meta name="yahooSeeker" content=" index, follow " />
+
+<meta name="msnbot" content=" index, follow " />
+
+<meta name="reply-to" content="shalimar@shalimar.org" />
+
+<meta name="allow-search" content="yes" />
+
+<meta name="revisit-after" content="daily" />
+
+<meta name="rating" content="General"/>
+
+<meta name="distribution" content="global" />
+
+<meta name="expires" content="never" />
+
+<meta http-equiv="content-language" content="english" /> 
+
+<link rel="stylesheet" type="text/css" href="style/themes.css"/>
+
+<link rel="stylesheet" type="text/css" href="style/responsive.css"/>
+
+<link rel="stylesheet" type="text/css" href="style/responsiveslides.css"/> 
+
+<link rel="stylesheet" type="text/css" href="style/main.css"/>
+
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="script/jquery-1.9.1.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+<script type="text/javascript" src="script/responsiveslides.js"></script>
+
+<script type="text/javascript" src="script/main.js"></script>
+
+<script type="text/javascript">
+
+$(window).load(function() {
+
+	$("#slider4").responsiveSlides({
+
+        auto: true,
+
+        pager: false,
+
+        nav: true,
+
+        speed: 500,
+
+        namespace: "callbacks",
+
+        before: function () {
+
+          $('.events').append("<li>before event fired.</li>");
+
+        },
+
+        after: function () {
+
+          $('.events').append("<li>after event fired.</li>");
+
+        }
+
+      });
+
+});
+
+</script>
+
+<style type="">
+
+  .button1 {
+
+  background-color: #b29630;
+
+  border: none;
+
+  color: white;
+
+  padding: 15px 32px;
+
+  text-align: center;
+
+  text-decoration: none;
+
+  display: inline-block;
+
+  font-size: 16px;
+
+  margin: 4px 2px;
+
+  cursor: pointer;
+
+}
+
+@media only screen and (min-width: 756px) and (max-width: 1400px) {
+
+  .about-cont{display: none!important;}
+
+}
+
+</style>
+
+	<script type="text/javascript" src="script/source/jquery.fancybox.js?v=2.1.5"></script>
+
+	<link rel="stylesheet" type="text/css" href="script/source/jquery.fancybox.css?v=2.1.5" media="screen" /> 
+
+
+
+<style>
+
+	body { overflow:auto;}
+
+</style>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-36181783-9"></script>
+
+<script>
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag(){dataLayer.push(arguments);}
+
+  gtag('js', new Date());
+
+  gtag('config', 'UA-36181783-9');
+
+</script>
+
+<!-- Facebook Pixel Code -->
+
+<script>
+
+  !function(f,b,e,v,n,t,s)
+
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+
+  n.queue=[];t=b.createElement(e);t.async=!0;
+
+  t.src=v;s=b.getElementsByTagName(e)[0];
+
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+
+  'https://connect.facebook.net/en_US/fbevents.js');
+
+  fbq('init', '656816088123736');
+
+  fbq('track', 'PageView');
+
+</script>
+
+<noscript><img height="1" width="1" style="display:none"
+
+  src="https://www.facebook.com/tr?id=656816088123736&ev=PageView&noscript=1"
+
+/></noscript>
+
+<!-- End Facebook Pixel Code -->
+
+
+
+
+
+<script>
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag(){dataLayer.push(arguments);}
+
+gtag('js', new Date());
+
+gtag('config', 'AW-ERRORCode');
+
+</script>
+<script type="application/ld+json">
+{
+"@context": "http://schema.org",
+"@type": "Organization",
+"name": "Shalimar Corp Limited",
+"description": "Shalimar was founded on the principles of knowledge, efficiency in management, and transparency, and is well-positioned to leverage and contribute to the growth and development of each of these sectors. With its headquarters in Lucknow, India, the Group’s footprints span across sectors such as real estate, property management, various allied services, civil construction, imports & exports, and glass processing. The group's trademark is a growing and expanding business portfolio which is steadily positioning the group as a market-leading business conglomerate.
+
+",
+"image": "https://www.shalimarcorp.com/images/banner/1366x768/02.jpg",
+"logo": "https://www.shalimarcorp.com/images/shalimar-logo.png",
+"url": "https://www.shalimarcorp.com/",
+"telephone": "522 - 4030444",
+"sameAs": ["https://twitter.com/shalimarcorpltd","https://www.linkedin.com/company/shalimarcorpltd/","https://www.facebook.com/shalimarcorpltd","https://www.instagram.com/shalimar_corp/"],
+"address": {
+"@type": "PostalAddress",
+"streetAddress": "11th Floor, Titanium, Shalimar Corporate Park, Vibhuti Khand, Gomti Nagar Lucknow 226010",
+"addressLocality": "Lucknow",
+"postalCode": "226010",
+"addressCountry": "India"
+}
+}
+</script>
+</head>
+
+<body>
+<!-- FOOTER -->
+
+<!-- FOOTER -->
+<div id="header">
+    <div id="logo">
+    	<a href="https://www.shalimarcorp.com/"><img src="images/shalimar-logo.png" /></a>
+    </div>
+    <div id="head_links">
+    	<!--<span class="text_call">Call : 1800-597-4658</span>-->
+        <a class="phone_square" href="tel:1800-597-4658"><i class="fa fa-phone-square"></i></a>
+    	<!--<a  href="javascript:void(0)" class="show_hide">Property Finder</a> <span>|</span>-->
+        <a href="https://www.shalimarcorp.com/blog.php">Blog</a> <span>|</span>
+    	<a href="https://www.shalimarcorp.com/careers.php">Careers</a> <span>|</span>
+    	<a href="https://www.shalimarcorp.com/contact-us.php">Contact Us</a>
+    </div>
+    <div id="mobile_menu">
+    	<i class="fa fa-bars"> MENU</i>
+    </div>
+    <div id="menu_bar">
+    	<div class="main_menu_list">
+            <a href="https://www.shalimarcorp.com/">HOME</a> <span>&bull;</span>
+  <div class="dropdown"><a class="drop_about_us" id="about-drop" href="#">ABOUT US <i class="fa fa-angle-down"></i></a> <span>&bull;</span>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="https://www.shalimarcorp.com/introduction.php">Introduction</a>
+            <a href="https://www.shalimarcorp.com/the-brand.php">The Brand</a>
+            <a href="https://www.shalimarcorp.com/vision-and-mission.php">Vision & Mission</a>
+            <a href="https://www.shalimarcorp.com/team.php">Team</a>
+          <!-- <a class="dropdown" href="rera.php">Rera  <i class="fa fa-angle-down"></i></a>-->
+ <div class="dropdown1">
+  <button class="dropbtn1">RERA   <i class="fa fa-angle-down"></i></button>
+  <div class="dropdown-content1">
+       <a href="https://www.shalimarcorp.com/rera-projects.php">Projects</a>
+        <a href="https://www.shalimarcorp.com/rera-promoter.php">Promoter</a>
+  </div>
+</div>
+            <a href="https://www.shalimarcorp.com/awards.php">Awards</a>
+            <a href="https://www.shalimarcorp.com/residential-ongoing-projects.php">Operational Focus </a>
+            <!-- <a href="https://www.shalimarcorp.com/csr-policy.php">CSR Policy</a> -->
+            <div class="dropdown1">
+              <button class="dropbtn1">POLICIES<i class="fa fa-angle-down"></i></button>
+              <div class="dropdown-content1">
+                  <a href="https://www.shalimarcorp.com/csr_policy_new.php">CSR Policy</a>
+                  <a href="https://www.shalimarcorp.com/nrc_policy.php">NRC Policy</a>
+                  <a href="https://www.shalimarcorp.com/vigil_mechanism.php">Vigil Mechanism</a>
+                  <a href="https://www.shalimarcorp.com/term_condition.php">Term & Condition</a>
+              </div>
+            </div>
+            <a href="https://www.shalimarcorp.com/other-verticals.php">Other Verticals</a>
+            <a href="https://www.shalimarcorp.com/testimonials.php">Testimonials</a>
+             <div class="dropdown1">
+              <button class="dropbtn1">REPORTS<i class="fa fa-angle-down"></i></button>
+              <div class="dropdown-content1">
+                 <a href="https://www.shalimarcorp.com/pdf/ANS-MGT-9.pdf" target="_blank">ANS </a>
+                  <a href="https://www.shalimarcorp.com/pdf/MGT-9-SCL-19-20.pdf" target="_blank">SCL</a>
+              </div>
+            </div>
+  </div> </div>
+<div onclick="myFunction()" class="dropdown"><a class="drop_about_us"  id="project-drop" href="#">PROJECTS <i class="fa fa-angle-down"></i></a> <span>&bull;</span>
+  <div class="dropdown-content" id="projects-id_dropdown">
+    <a href="https://www.shalimarcorp.com/residential-ongoing-projects.php">Residential</a>
+            <a href="https://www.shalimarcorp.com/commercial-ongoing-projects.php">Commercial</a>
+            <a href="https://www.shalimarcorp.com/completed-projects.php">Completed</a>
+            <a href="https://www.shalimarcorp.com/government-projects.php">Government</a>
+            </div>
+            </div>
+        <div class="dropdown"><a class="drop_about_us " id="media-drop" href="#">MEDIA <i class="fa fa-angle-down"></i></a> <span>&bull;</span>
+  <div class="dropdown-content"  id="media-id_dropdown">
+   <a href="https://www.shalimarcorp.com/press-coverage.php">Press Coverage</a>
+            <a href="news-events.php">News & Events</a>
+            </div>
+            </div>
+  <div class="dropdown"><a class="drop_about_us" id="know-drop" href="#">KNOWLEDGE CENTRE <i class="fa fa-angle-down"></i></a> <span>&bull;</span>
+  <div class="dropdown-content" id="know-id_dropdown">
+   <a href="https://www.shalimarcorp.com/buyers-guide.php">Buyers Guide</a>
+            <a href="https://www.shalimarcorp.com/home-loans-facts.php">Home Loans Facts</a>
+            <a href="https://www.shalimarcorp.com/tools.php">Tools</a>
+            <a href="https://www.shalimarcorp.com/nri-faqs.php">NRI FAQ’s</a>
+            </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3939850173581632"
+     data-ad-slot="3604706469"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<!-- HEADER CLOSE -->
+<div class="slidingDiv request-visit" style="display: block;">
+<div class="header_banner" style="position:static; background:#f7f6ee; padding:60px 0;">
+	<div class="header_box"  style="position:static">
+        <div class="header_content project_header" style="margin:0px auto !important; ">
+            <h2 class="property_finder">Property Finder</h2>
+            <hr />
+            <p>Find your dream lifespace</p>
+            <div class="property_filter">
+            	<select>
+                	<option>Select Location</option>
+                </select>
+            	<select>
+                	<option>Select Apt. Type</option>
+                </select>
+            	<select>
+                	<option>Select Budget</option>
+                </select>
+                <input type="submit" name="submit" value="Search" />
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="callbacks_container">
+
+    <ul class="rslides" id="slider4">
+   <li><a href="http://shalimar-oneworld.com/belvederesuites/" target="_blank"><img id="img6" src="images/banner/1366x768/Web-Banner-2.jpg" alt="Construction Company in Lucknow"></a></li>  
+  <li><img id="img0" src="images/banner/1366x768/00.jpg" alt="Construction Company in Lucknow"></li>
+
+ <li><a href="https://www.shalimarcorp.com/mannat-extension/" target="_blank"><img id="img6" src="images/banner/1366x768/07.jpg" alt="Construction Company in Lucknow"></a></li> 
+
+  <li><a href="http://shalimar-oneworld.com/belvedere-3/" target="_blank"><img id="img1" src="images/banner/1366x768/01.jpg" alt="Construction Company in Lucknow"></a></li>
+
+  <li>
+
+    <a href="https://www.shalimarcorp.com/garden-bay-aster/" target="_blank">
+
+    <img id="img7" src="images/banner/1366x768/001.jpg" alt="Construction Company in Lucknow">
+
+  </a></li>
+
+     <li><img id="img2" src="images/banner/1366x768/02.jpg" alt="Construction Company in Lucknow"></li>
+
+      <li><img id="img3" src="images/banner/1366x768/03.jpg" alt="Construction Company in Lucknow"></li>
+
+      <li><img id="img4" src="images/banner/1366x768/04.jpg" alt="Construction Company in Lucknow"></li>
+
+      <li><img id="img5" src="images/banner/1366x768/05.jpg" alt="Construction Company in Lucknow"></li>
+
+     <li><img id="img6" src="images/banner/1366x768/06.jpg" alt="Construction Company in Lucknow"></li>
+
+    </ul>
+
+</div><!--End Slide 1-->
+
+<div class="content_box project_box about-cont" style="background-color: #fce697;">
+
+  <div id="overview" class="project_menu_title" style="text-align: center; padding-top: 30px;"> 
+
+      <h1>Welcome to Shalimar</h1>
+
+    </div>
+
+    <div class="" style="text-align: center;">
+
+        <p style="text-align: center !important; font-size: 14px; color: #000;">Established in 1985, the Shalimar Group, popularly known as luxury property builders in Lucknow is a diversified Business House headquartered in Lucknow, India. The Group’s footprints span sectors such as Real Estate, Property Management, various Allied Services, Civil Construction, Imports & Exports and Glass Processing. </p>
+
+        <a href="https://www.shalimarcorp.com/introduction.php" class="button1 ">Read More</a><br/><br/><br/>
+
+    </div>
+
+</div>
+<div id="footer">
+
+	<div class="left" style="Width:60%">
+    
+        &copy; 2018 &nbsp; &nbsp; &nbsp; &nbsp;
+
+        <a href="https://www.facebook.com/shalimarcorpltd" target="_blank"><i class="fa fa-facebook"></i></a><a href="https://twitter.com/shalimarcorpltd" target="_blank"><i class="fa fa-twitter"></i></a><a href="https://www.instagram.com/shalimar_corp/" target="_blank"><i class="fa fa-instagram"></i></a>
+
+        <a href="https://www.linkedin.com/company/shalimarcorpltd/?viewAsMember=true" target="_blank"><i class="fa fa-linkedin"></i></a>
+    </div>
+</div>
+<!-- FOOTER CLOSE -->
+<script>
+$(document).ready(function(){  
+$(".slidingDiv").hide();
+$(".show_hide").show(); 
+$('.show_hide').click(function(){
+$(".slidingDiv").slideToggle();
+});
+
+});
+</script>
+<!-- FOOTER CLOSE -->
+
+<!-- HEADER -->
+<!-- HEADER -->
+
+
+<script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+   document.getElementById("myDropdown").classList.toggle("show");
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+$(document).ready(function(){
+$("#about-drop").click(function(){
+$("#myDropdown").toggle();
+});
+$("#project-drop").click(function(){
+$("#projects-id_dropdown").toggle();
+});
+$("#media-drop").click(function(){
+$("#media-id_dropdown").toggle();
+});
+$("#know-drop").click(function(){
+$("#know-id_dropdown").toggle();
+});
+});
+</script>
+
+<?php include 'footer.php'; ?>
+        
+</body>
+
+</html>
