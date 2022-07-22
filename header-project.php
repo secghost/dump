b6af72b56bc8c0c27d5bbc168a50449bac4ed3ed
Sec Ghost <sec666host@gmail.com>
2022-07-22 13:45:40 +0700

    Create header-project.php

---
+++ header-project.php
@@ -0,0 +1,256 @@
+<style>
+    .google-auto-placed {
+    display: none !important;
+}
+</style>
+
+<!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?4agNmUgtb0ez1lvKwTt3sMNI22S7N3ZZ";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zendesk Chat Script-->
+
+<!-- HEADER -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3939850173581632" crossorigin="anonymous"></script>
+<div id="header">
+
+
+
+    <div id="logo">
+
+    	<a href="index.php"><img src="images/shalimar-logo.png" /></a>
+
+    </div>
+
+    
+
+    <div id="head_links">
+
+    	<!--<span class="text_call">Call : 1800-597-4658</span>-->
+
+        <a class="phone_square" href="tel:1800-597-4658"><i class="fa fa-phone-square"></i></a>
+
+    	<!--<a  href="javascript:void(0)" class="show_hide">Property Finder</a> <span>|</span>-->
+
+        <a href="https://www.shalimarcorp.com/blog.php">Blog</a> <span>|</span>
+
+    	<a href="https://www.shalimarcorp.com/contact-us.php">Careers</a> <span>|</span>
+
+    	<a href="https://www.shalimarcorp.com/careers.php">Contact Us</a>
+
+    </div>
+
+    
+
+    <div id="mobile_menu">
+
+    	<i class="fa fa-bars"> MENU</i>
+
+    </div>
+
+    <div id="menu_bar">
+
+    	<div class="main_menu_list">
+
+            <a href="https://www.shalimarcorp.com/">HOME</a> <span>&bull;</span>
+            
+  <div class="dropdown"><a class="drop_about_us" id="about-drop" href="#">ABOUT US <i class="fa fa-angle-down"></i></a> <span>&bull;</span>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="https://www.shalimarcorp.com/introduction.php">Introduction</a>
+
+            <a href="https://www.shalimarcorp.com/the-brand.php">The Brand</a>
+
+            <a href="https://www.shalimarcorp.com/vision-and-mission.php">Vision & Mission</a>
+
+            <a href="https://www.shalimarcorp.com/team.php">Team</a>
+          
+          <!-- <a class="dropdown" href="rera.php">Rera  <i class="fa fa-angle-down"></i></a>-->
+ <div class="dropdown1">
+
+  <button class="dropbtn1">RERA   <i class="fa fa-angle-down"></i></button>
+
+  <div class="dropdown-content1">
+
+       <a href="https://www.shalimarcorp.com/rera-projects.php">Projects</a>
+
+        <a href="https://www.shalimarcorp.com/rera-promoter.php">Promoter</a>
+
+  </div></div>
+
+            <a href="https://www.shalimarcorp.com/awards.php">Awards</a>
+
+            <a href="https://www.shalimarcorp.com/residential-ongoing-projects.php">Operational Focus </a>
+
+            <a href="https://www.shalimarcorp.com/csr-policy.php">CSR Policy</a>
+
+            <a href="https://www.shalimarcorp.com/other-verticals.php">Other Verticals</a>
+
+            <a href="https://www.shalimarcorp.com/testimonials.php">Testimonials</a>
+            
+            
+  </div> </div>
+
+<div onclick="myFunction()" class="dropdown"><a class="drop_about_us"  id="project-drop" href="#">PROJECTS <i class="fa fa-angle-down"></i></a> <span>&bull;</span>
+  <div class="dropdown-content" id="projects-id_dropdown">
+    <a href="https://www.shalimarcorp.com/residential-ongoing-projects.php#">Residential</a>
+
+            <a href="https://www.shalimarcorp.com/commercial-ongoing-projects.php">Commercial</a>
+
+            <a href="https://www.shalimarcorp.com/completed-projects.php">Completed</a>
+
+            <a href="https://www.shalimarcorp.com/government-projects.php">Government</a>
+            </div>
+            </div>
+        
+        <div class="dropdown"><a class="drop_about_us " id="media-drop" href="#">MEDIA <i class="fa fa-angle-down"></i></a> <span>&bull;</span>
+  <div class="dropdown-content"  id="media-id_dropdown">
+   <a href="https://www.shalimarcorp.com/press-coverage.php">Press Coverage</a>
+
+            <a href="https://www.shalimarcorp.com/news-events.php">News & Events</a>
+            </div>
+            </div>
+        
+  <div class="dropdown"><a class="drop_about_us" id="know-drop" href="#">KNOWLEDGE CENTRE <i class="fa fa-angle-down"></i></a> <span>&bull;</span>
+  <div class="dropdown-content" id="know-id_dropdown">
+   <a href="https://www.shalimarcorp.com/buyers-guide.php">Buyers Guide</a>
+
+            <a href="https://www.shalimarcorp.com/home-loans-facts.php">Home Loans Facts</a>
+
+            <a href="https://www.shalimarcorp.com/tools.php">Tools</a>
+
+            <a href="https://www.shalimarcorp.com/nri-faqs.php">NRI FAQ’s</a>
+            </div>
+            </div>
+         
+        </div>
+        
+
+<!--
+          <div class="sub_menu sub_menu_rera" style="display:none;">
+
+        	<a class="back_knowledge" href="#"><i class="fa fa-reply"> &nbsp; Back</i></a>
+
+        	<a href="buyers-guide.php">Buyers Guide</a>
+
+      <a href="tax-benefits.php">Tax Benefits</a>
+       <a href="home-loans-facts.php">Home Loans Facts</a>
+
+            <a href="tools.php">Tools</a>
+
+            <a href="nri-faqs.php">NRI FAQ’s</a>
+
+        </div>-->
+
+    </div>
+
+</div>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3939850173581632"
+     data-ad-slot="3604706469"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<!-- HEADER CLOSE -->
+
+
+
+<div class="slidingDiv request-visit" style="display: block; height: 300px;">
+
+<div class="header_banner" style="position:static; background:#f7f6ee; padding:60px 0;">
+
+	<div class="header_box"  style="position:static">
+
+        <div class="header_content project_header" style="margin:0px auto !important; ">
+
+            <h1>Property Finder</h1>
+
+            <hr />
+
+            <p>Find your dream lifespace</p>
+
+            <div class="property_filter">
+
+            	<select>
+
+                	<option>Select Location</option>
+
+                </select>
+
+            	<select>
+
+                	<option>Select Apt. Type</option>
+
+                </select>
+
+            	<select>
+
+                	<option>Select Budget</option>
+
+                </select>
+
+                <input type="submit" name="submit" value="Search" />
+
+            </div>
+
+            <div class="clear"></div>
+
+        </div>
+
+        
+
+    </div>
+
+ 
+
+</div>
+
+
+
+</div>
+
+
+<script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+   document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+$(document).ready(function(){
+$("#about-drop").click(function(){
+$("#myDropdown").toggle();
+});
+$("#project-drop").click(function(){
+$("#projects-id_dropdown").toggle();
+});
+$("#media-drop").click(function(){
+$("#media-id_dropdown").toggle();
+});
+$("#know-drop").click(function(){
+$("#know-id_dropdown").toggle();
+});
+});
+</script>
